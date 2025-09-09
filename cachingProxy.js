7e9092f469f1165b2c05fcf45a4411e9720b4927
theagentpenguin <gunaseelanabcd@gmail.com>
2025-09-09 22:50:32 +0530

    removing node modules

--- cachingProxy.js
+++ cachingProxy.js
@@ -4,6 +4,13 @@ import { createSpinner } from 'nanospinner';
 import { createProxy } from 'proxy';
 import * as http from 'http';
 
+/*
+We'll be using the inquirer to get the values from the CLI and then check our stack for the URL.
+If the URL is present in the stack, we will return a HIT, if not, MISS.
+
+
+*/
+
 const server = createProxy(http.createServer());
 server.listen(3030, ()=>{
     var port = server.address().port;
