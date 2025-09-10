f15ba8e6d1ef41d06fa191a6e19df09ac03b9d33
theagentpenguin <gunaseelanabcd@gmail.com>
2025-09-10 23:52:16 +0530

    Basic functionality is achieved. It is to be updated to meet the exact client requirement

--- cachingProxy.js
+++ cachingProxy.js
@@ -1,19 +1,40 @@
 import chalk from 'chalk';
 import chalkAnimation from 'chalk-animation';
 import { createSpinner } from 'nanospinner';
-import { createProxy } from 'proxy';
-import * as http from 'http';
+import inquirer from 'inquirer';
 
+const urlStack = [];
 /*
 We'll be using the inquirer to get the values from the CLI and then check our stack for the URL.
 If the URL is present in the stack, we will return a HIT, if not, MISS.
 
 
 */
+async function welcomeUser(){
+    const welcomeMessage = chalkAnimation.karaoke(
+            'Enter the url, lets check if it is in cache! \n'
+        );
+}
+async function getInputUrl(){
 
-const server = createProxy(http.createServer());
-server.listen(3030, ()=>{
-    var port = server.address().port;
-    console.log('HTTPs server is up and is running on port %d', port);
-});
+    const {input_url} = await inquirer.prompt({
+        name:'input_url',
+        type: 'input'
+    });
+    const spinner = createSpinner('Lets check it!!!');
+    if(urlStack.includes(input_url)){
+        chalk.blue(console.log("HIT"));
+    }else{
+        urlStack.push(input_url);
+        chalk.blue(console.log("MISS"));
+    }
+    console.log(urlStack);
+}
 
+// const server = createProxy(http.createServer());
+// server.listen(3030, ()=>{
+//     var port = server.address().port;
+//     console.log('HTTPs server is up and is running on port %d', port);
+// });
+await welcomeUser();
+await getInputUrl();
